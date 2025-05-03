995af080f4a792589631460c4bafcfe20df5418b
Bern0va <aucor.limited@gmail.com>
2025-05-03 20:21:56 +0300

    Create ronix-icon-resize.lua

---
+++ ronix-icon-resize.lua
@@ -0,0 +1,2 @@
+game:GetService("CoreGui").Folder.RoniXUI.RonixButton.Size = UDim2.new(0.100000009, 0, 0.100000024, 0)
+game:GetService("CoreGui").Folder.RoniXUI.RonixButton.UICorner.CornerRadius = UDim.new(0.3, 0.1)
